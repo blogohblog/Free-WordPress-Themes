af75c44edc595fad6d374147437a0ceb4e8dac2c
Elio Rivero <eliorivero@gmail.com>
2015-06-04 08:35:33 -0300

    I18n, correct markup and phpdoc for sidebar template.

--- keepwriting/sidebar.php
+++ keepwriting/sidebar.php
@@ -1,5 +1,19 @@
-    <ul class="sidebar">
-    <?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar('bottomsection') ) : else : ?>
-    <p>Please configure this section through Widgets</p>        
-    <?php endif; ?>
-    </ul>
+<?php
+/**
+ * Sidebar template
+ *
+ * @package WordPress
+ * @subpackage keepwriting
+ * @since keepwriting 1.0
+ */
+?>
+
+<ul class="sidebar">
+
+	<?php if ( function_exists( 'dynamic_sidebar' ) && dynamic_sidebar( 'bottomsection' ) ) : else : ?>
+
+		<li><p><?php _e( 'Please configure this section through Widgets', 'keepwriting' ); ?></p></li>
+
+	<?php endif; ?>
+
+</ul>
